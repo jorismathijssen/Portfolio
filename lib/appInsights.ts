ede3e1df774a875c407c1562c5c991d8a24a1d6e
Joris <jorismathijssen@gmail.com>
2024-10-21 00:06:13 +0200

    Added tracking to learn from

--- lib/appInsights.ts
+++ lib/appInsights.ts
@@ -1,11 +1,40 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
+import { createBrowserHistory } from "history";
+import { ClickAnalyticsPlugin } from '@microsoft/applicationinsights-clickanalytics-js';
 
-const appInsights = new ApplicationInsights({
-  config: {
-    instrumentationKey: process.env.NEXT_PUBLIC_APPLICATIONINSIGHTS_CONNECTION_STRING,
-  },
-});
+let appInsights: ApplicationInsights;
 
-appInsights.loadAppInsights();
+if (typeof window !== 'undefined') {
+  const browserHistory = createBrowserHistory();
+  const reactPlugin = new ReactPlugin();
+
+  const clickPluginInstance = new ClickAnalyticsPlugin();
+  const clickPluginConfig = {
+    autoCapture: true,
+    dataTags: {
+      useDefaultContentNameOrId: true
+    }
+  };
+
+  appInsights = new ApplicationInsights({
+    config: {
+      connectionString: process.env.NEXT_PUBLIC_APPLICATIONINSIGHTS_CONNECTION_STRING,
+      enableAutoRouteTracking: true,
+      extensions: [reactPlugin, clickPluginInstance],
+      extensionConfig: {
+        [reactPlugin.identifier]: { history: browserHistory },
+        [clickPluginInstance.identifier]: clickPluginConfig
+      }
+    },
+  });
+
+  appInsights.loadAppInsights();
+} else {
+  appInsights = {
+    trackPageView: () => {},
+    trackEvent: () => {},
+  } as unknown as ApplicationInsights;
+}
 
 export default appInsights;
